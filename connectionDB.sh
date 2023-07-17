59279e1a034d90475ad2697286d5191204d6eea1
Deepak Nishad <deepaknishad@Deepaks-MacBook-Air.local>
2023-07-17 23:21:32 +0530

    changes after review

--- connectionDB.sh
+++ connectionDB.sh
@@ -4,23 +4,9 @@ CONNECTION_FILE='connection.txt'
 DETAILED_CONNECTION_FILE='detailed_connection.txt'
 
 store_connection_details() {
-    echo "number of args $#"
     
-    if [ $# -eq 2 ]; then
-        echo "${1}" >> ${CONNECTION_FILE}
-        echo "${1}: ssh ${USER_NAME_SERVER_IP}" >> ${DETAILED_CONNECTION_FILE}
-
-    elif [ $# -eq 3 ]; then
-        echo "${1}" >> ${CONNECTION_FILE}
-        echo "${1}: ssh -p ${PORT} ${USER_NAME_SERVER_IP}" >> ${DETAILED_CONNECTION_FILE}
-        
-    elif [ $# -eq 4 ]; then
-        echo "${1}" >> ${CONNECTION_FILE}
-        echo "${1}: ssh -i ${PEM_FILE} -p ${PORT} ${USER_NAME_SERVER_IP}" >> ${DETAILED_CONNECTION_FILE}
-
-    else
-        echo "Invalid operration.."
-    fi
-
+     echo "${SERVER_NAME}:${PEM_FILE}:${PORT}:${USER_NAME}:${HOST}" >> ${CONNECTION_FILE}
 
 }
+
+#server:file:port:user:ip
--- otssh.sh
+++ otssh.sh
@@ -11,7 +11,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' 
 
-
 flag_a=false
 flag_l=false
 flag_m=false
@@ -21,44 +20,34 @@ flag_d=false
 while getopts "an:h:u:p:ld:mn:r:i:" opt; do
   case $opt in
     a)
-      echo "Option 'a' is set."
       flag_a=true
       ;;
     n)
-      echo "Option 'n' is set."
       n=$OPTARG
       ;;
     h)
-      echo "Option 'h' is set."
       h=$OPTARG
       ;;
     u)
-      echo "Option 'u' is set."
       u=$OPTARG
       ;;
     p)
-      echo "Option 'p' is set."
       p=$OPTARG
       ;;
     l)
-      echo "Option 'l' is set."
       flag_l=true
       ;;
     d)
-      echo "Option 'd' is set."
       flag_d=true
       ;;
     m)
-      echo "Option 'm' is set."
       flag_m=true
       ;;
     r)
-      echo "Option 'r' is set."
       flag_r=true
       r=$OPTARG
       ;;
     i)
-      echo "Option 'i' is set."
       i=$OPTARG
       ;;
     \?)
@@ -68,40 +57,44 @@ while getopts "an:h:u:p:ld:mn:r:i:" opt; do
 done 2>/dev/null
 
 USER_NAME_SERVER_IP=${u}@${h}
+HOST=${h}
 USER_NAME=${u}
 SERVER_NAME=${n}
 PORT=${p}
 PEM_FILE=${i}
 SERVER_TO_REMOVE=${r}
 
-if $flag_a; then
+if  [ ${flag_a} = true ] && [ ${flag_l} = false ] && [ ${flag_m} = false ] && [ ${flag_r} = false ]; then
+
     if [ $flag_a ] && [ ! -z {$n} ] && [ ! -z ${h} ] && [ ! -z ${u} ] && [ -z ${p} ] && [ -z ${i} ]; then
-        ssh ${USER_NAME_SERVER_IP}
-        echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" >> ${LOG_FILE}
-        store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP}
+        ssh ${USER_NAME_SERVER_IP} 2>/dev/null
+        echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" | tee -a ${LOG_FILE}
+        store_connection_details ${SERVER_NAME} ${USER_NAME} ${HOST}
     
     elif [ $flag_a ] && [ ! -z {$n} ] && [ ! -z ${h} ] && [ ! -z ${u} ] && [ ! -z ${p} ] && [ -z ${i} ]; then
-        ssh ${USER_NAME_SERVER_IP} -p ${PORT}
-        echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" >> ${LOG_FILE}
-        store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP} ${PORT}
+        ssh ${USER_NAME_SERVER_IP} -p ${PORT} 2>/dev/null
+        echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" | tee -a ${LOG_FILE}
+        store_connection_details ${SERVER_NAME} ${USER_NAME} ${HOST} ${PORT}
 
     elif [ $flag_a ] && [ ! -z {$n} ] && [ ! -z ${h} ] && [ ! -z ${u} ] && [ ! -z ${p} ] && [ ! -z ${i} ]; then
-        ssh -p ${PORT} -i ${PEM_FILE} ${USER_NAME_SERVER_IP}  
-        echo -e "${GREEN}Connecting${NC} to ${SERVER_NAME} on ${PORT} port as ${USER_NAME} via ${PEM_FILE} key" >> ${LOG_FILE}
-        store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP} ${PORT} ${PEM_FILE}
+        ssh -p ${PORT} -i ${PEM_FILE} ${USER_NAME_SERVER_IP} 2>/dev/null
+        echo -e "${GREEN}Connecting${NC} to ${SERVER_NAME} on ${PORT} port as ${USER_NAME} via ${PEM_FILE} key" | tee -a ${LOG_FILE}
+        store_connection_details ${SERVER_NAME} ${USER_NAME} ${HOST} ${PORT} ${PEM_FILE}
     fi
 fi
 
-if $flag_l; then
+if [ ${flag_a} = false ] && [ ${flag_l} = true ] && [ ${flag_m} = false ] && [ ${flag_r} = false ]; then
 
     if [ ${NUMBER_OF_ARG} -eq 1 ]; then
-        cat ${CONNECTION_FILE}
+        awk -F: '{print $1}' ${CONNECTION_FILE}
     elif [ flag_d ]; then
-        cat ${DETAILED_CONNECTION_FILE}
+        awk -F: '{if(length($2) == 0 && length($3) == 0) print $1": ssh "$4"@"$5" "; else if(length($2) == 0) print $1": ssh -p " $3 " "$4"@"$5" "; else print $1": ssh -i " $2 " -p " $3 " "$4"@"$5""}' ${CONNECTION_FILE}
+
     fi
 fi
 
-if $flag_m; then
+if [ ${flag_a} = false ] && [ ${flag_l} = false ] && [ ${flag_m} = true ] && [ ${flag_r} = false ]; then
+
     if [ ! -z {$n} ] && [ ! -z ${h} ] && [ ! -z ${u} ] && [ -z ${p} ]; then
         sed -i -e "s/\(${SERVER_NAME}: ssh \).*/\1${USER_NAME_SERVER_IP}/" ${DETAILED_CONNECTION_FILE}
 
@@ -114,7 +107,8 @@ if $flag_m; then
 
 fi
 
-if $flag_r; then
+if [ ${flag_a} = false ] && [ ${flag_l} = false ] && [ ${flag_m} = false ] && [ ${flag_r} = true ]; then
+
     if [ ${NUMBER_OF_ARG} -eq 2 ]; then
         awk "!/${SERVER_TO_REMOVE}/" ${CONNECTION_FILE} > tmpfile && mv tmpfile ${CONNECTION_FILE} 
    
@@ -123,3 +117,5 @@ if $flag_r; then
     fi
 fi
 
+
+#awk -F: '{print "ssh -i " $2 " -p " $3 " "$4"@"$5}' ${CONNECTION_FILE}
