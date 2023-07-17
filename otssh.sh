d5912a6293980a37ec29589fcc82ef2cefc7e0b7
Deepak Nishad <deepaknishad@Deepaks-MacBook-Air.local>
2023-07-17 19:18:17 +0530

    segregated code

--- otssh.sh
+++ otssh.sh
@@ -11,67 +11,112 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' 
 
-case "$OPERATION" in
 
-   "-a")
+flag_a=false
+flag_l=false
+flag_m=false
+flag_r=false
+flag_d=false
 
-        USER_NAME_SERVER_IP=${7}@${5}
-        USER_NAME=${7}
-        SERVER_NAME=${3}
-        PORT=${9}
-        PEM_FILE=${11}
+while getopts "an:h:u:p:ld:mn:r:" opt; do
+  case $opt in
+    a)
+      echo "Option 'a' is set."
+      flag_a=true
+      ;;
+    n)
+      echo "Option 'n' is set."
+      n=$OPTARG
+      ;;
+    h)
+      echo "Option 'h' is set."
+      h=$OPTARG
+      ;;
+    u)
+      echo "Option 'u' is set."
+      u=$OPTARG
+      ;;
+    p)
+      echo "Option 'p' is set."
+      p=$OPTARG
+      ;;
+    l)
+      echo "Option 'l' is set."
+      flag_l=true
+      ;;
+    d)
+      echo "Option 'd' is set."
+      flag_d=true
+      ;;
+    m)
+      echo "Option 'm' is set."
+      flag_m=true
+      ;;
+    r)
+      echo "Option 'r' is set."
+      flag_r=true
+      ;;
+    i)
+      echo "Option 'i' is set."
+      i=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      ;;
+  esac
+done 2>/dev/null
 
-        if [ ${NUMBER_OF_ARG} -eq 7 ]; then
-            ssh ${USER_NAME_SERVER_IP}
-            echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" >> ${LOG_FILE}
-            store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP}
+echo "OPTION A=$a N=$n H=$h U=$u P=$p I=$i R=$r L=$l"
+USER_NAME_SERVER_IP=${u}@${h}
+USER_NAME=${u}
+SERVER_NAME=${n}
+PORT=${p}
+PEM_FILE=${i}
 
-        elif [ $NUMBER_OF_ARG -eq 9 ]; then
-            ssh ${USER_NAME_SERVER_IP} -p ${PORT}
-            echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" >> ${LOG_FILE}
-            store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP} ${PORT}
+if $flag_a; then
+    echo "Flag 'a' is used."
+    if [ $flag_a ] && [ ! -z {$n} ] && [ ! -z ${h} ] && [ ! -z ${u} ] && [ -z ${p} ] && [ -z ${i} ]; then
+        echo "performing add operation"
+        ssh ${USER_NAME_SERVER_IP}
+        echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" >> ${LOG_FILE}
+        store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP}
+    
+    elif [ $flag_a ] && [ ! -z {$n} ] && [ ! -z ${h} ] && [ ! -z ${u} ] && [ ! -z ${p} ] && [ -z ${i} ]; then
+        ssh ${USER_NAME_SERVER_IP} -p ${PORT}
+        echo -e "${RED}[ERROR]:${NC} ${SERVER_NAME} information is not available, please add server first" >> ${LOG_FILE}
+        store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP} ${PORT}
 
-        elif [ $NUMBER_OF_ARG -eq 11 ]; then
-            ssh -p ${PORT} -i ${PEM_FILE} ${USER_NAME_SERVER_IP}  
-            echo -e "${GREEN}Connecting${NC} to ${SERVER_NAME} on ${PORT} port as ${USER_NAME} via ${PEM_FILE} key" >> ${LOG_FILE}
-            store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP} ${PORT} ${PEM_FILE}
+    elif [ $flag_a ] && [ ! -z {$n} ] && [ ! -z ${h} ] && [ ! -z ${u} ] && [ ! -z ${p} ] && [ ! -z ${-i} ]; then
+        ssh -p ${PORT} -i ${PEM_FILE} ${USER_NAME_SERVER_IP}  
+        echo -e "${GREEN}Connecting${NC} to ${SERVER_NAME} on ${PORT} port as ${USER_NAME} via ${PEM_FILE} key" >> ${LOG_FILE}
+        store_connection_details ${SERVER_NAME} ${USER_NAME_SERVER_IP} ${PORT} ${PEM_FILE}
+    fi
+fi
 
-        else
-            echo "please provide all required information..!"
-        fi
-        ;;
+if $flag_l; then
+    echo "Flag 'l' is used"
+    if [ ${NUMBER_OF_ARG} -eq 1 ]; then
+        cat ${CONNECTION_FILE}
+    elif [ flag_d ]; then
+        cat ${DETAILED_CONNECTION_FILE}
+    fi
+fi
 
-    "-l")
-        if [ ${NUMBER_OF_ARG} -eq 1 ]; then
-            cat ${CONNECTION_FILE}
-
-        elif [ $NUMBER_OF_ARG -eq 2 ]; then
-            cat ${DETAILED_CONNECTION_FILE}
-
-        else
-            echo "incorrect arguments are passed..!"
-        fi
-    ;;
-
-    "-r")
-        SERVER_NAME=${2}
-
-        if [ ${NUMBER_OF_ARG} -eq 1 ]; then
-        awk "!/${SERVER_NAME}/" ${CONNECTION_FILE} > tmpfile && mv tmpfile ${CONNECTION_FILE} 
-
-        else
-            echo "incorrect arguments are passed..!"
-        fi
-    ;;
-
-    "-m")
-
-    SERVER_NAME=${3}
-    USER_NAME=${7}
-
+if $flag_m; then
+    echo "Flag 'm' is used "
     if [ ${NUMBER_OF_ARG} -eq 7 ]; then
-    sed -i -e "s/\(server1: ssh \).*/\1user1@192.168.42.35/" detailed_connection.txt 
-esac
+        sed -i -e "s/\(${SERVER_NAME}: ssh \).*/\1${USER_NAME_WITH_IP}/" ${DETAILED_CONNECTION_FILE}
 
+    elif [ $NUMBER_OF_ARG -eq 9 ]; then
+        sed -i -e "s/\(${SERVER_NAME}: ssh -p 2022 \).*/\1${USER_NAME_WITH_IP}/" ${DETAILED_CONNECTION_FILE} 
 
-#sed -i -e "s/\(server2: ssh -p 2022 \).*/\1user2@192.168.42.38/" detailed_connection.txt 
+    else
+        echo "incorrect arguments are passed..!"
+    fi
+
+fi
+
+if $flag_r; then
+  echo "Flag 'r' is used with value: $username"
+fi
+
--- trail.sh
+++ trail.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
-while getopts a:b:c: opt; do
+flag_a=false
+flag_l=false
+flag_m=false
+flag_r=false
+
+while getopts "an:h:u:p:ld:mn:r:" opt; do
   case $opt in
     a)
       echo "Option 'a' is set."
       a=$OPTARG
+      flag_a=true
       ;;
-    b)
-      echo "Option 'b' is set with value: $OPTARG"
-      b=$OPTARG
+    n)
+      echo "Option 'n' is set."
+      n=$OPTARG
       ;;
-    c)
+    h)
       echo "Option 'c' is set."
-      c=$OPTARG
+      h=$OPTARG
+      ;;
+    u)
+      echo "Option 'c' is set."
+      u=$OPTARG
+      ;;
+    p)
+      echo "Option 'c' is set."
+      p=$OPTARG
+      ;;
+    l)
+      echo "Option 'l' is set."
+      l=$OPTARG
+      flag_l=true
+      ;;
+    d)
+      echo "Option 'd' is set."
+      d=$OPTARG
+      ;;
+    m)
+      echo "Option 'm' is set."
+      m=$OPTARG
+      flag_m=true
+      ;;
+    r)
+      echo "Option 'r' is set."
+      r=$OPTARG
+      flag_r=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG"
@@ -20,4 +53,23 @@ while getopts a:b:c: opt; do
   esac
 done
 
-echo "OPTION A=$a B=$b C=$c"
+echo "OPTION A=$a N=$n H=$h U=$u P=$p R=$r L=$l"
+
+if $flag_a; then
+  echo "Flag 'a' is used."
+    if [ $flag_a ] && [ ! -z ${n} ] && [ ! -z ${p} ]; then
+    echo "performing add operation"
+    fi
+fi
+
+if $flag_l; then
+  echo "Flag 'l' is used with value: $server_name"
+fi
+
+if $flag_m; then
+  echo "Flag 'm' is used with value: $server_host"
+fi
+
+if $flag_r; then
+  echo "Flag 'r' is used with value: $username"
+fi
